949ff5ad8771b42b3716b5130c34f4c1affb1a23
Amol Shinde <shindeamol409@gmail.com>
2024-10-05 20:22:23 +0530

    Designed exemplary schema with mongoose for ecommerce

---
+++ 3.Data Modelling for backend with mongoose/models/ecommerce/order.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    unique: true,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: {
+      type: [orderItemSchema],
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    orderStatus: {
+      type: String,
+      enum: ["PENDING", "CANCELLED", "DELIVERED"],
+      default: "PENDING",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
